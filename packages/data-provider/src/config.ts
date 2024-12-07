e34a604c300a8bc9ebb6653700e98a77f3920ed4
wsybok <wsybok@gmail.com>
2024-12-07 13:02:05 +0800

    Update config.ts

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -749,6 +749,7 @@ export const supportsBalanceCheck = {
   [EModelEndpoint.azureAssistants]: true,
   [EModelEndpoint.azureOpenAI]: true,
   [EModelEndpoint.bedrock]: true,
+  [EModelEndpoint.google]: true,
 };
 
 export const visionModels = [
